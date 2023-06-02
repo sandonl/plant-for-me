a519d3d3f3c560536fe3f2987d562352cedaae9d
Sandon Lai <sandonnlai@gmail.com>
2023-06-02 16:53:58 +1000

    feat: individual pages for plants

--- src/pages/plants/[id].tsx
+++ src/pages/plants/[id].tsx
@@ -1,22 +1,28 @@
 import Layout from "@/src/components/Layout";
+import { Button } from "@/src/components/ui/button";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 interface PlantPage {}
 
 const PlantPage = ({}: PlantPage) => {
   const router = useRouter();
+  const { name, plantName, water, id } = router.query;
 
-  console.log(router.query);
+  console.log(id);
 
-  const { name, plantName, water } = router.query;
-
-  console.log(name, plantName, water);
+  // Could just query and reutrn the page based on the id
 
   return (
     <>
-      <Layout pageTitle={"Plant"}>
-        <h3> This is a specific plant page </h3>
-        <> {name} </>
+      <Layout pageTitle={name as string}>
+        <div className="flex min-w-full flex-col items-center justify-center py-8">
+          <h1 className="text-sm font-bold md:text-3xl"> {name} </h1>
+          <h2 className="py-2"> {plantName} </h2>
+          <Button asChild>
+            <Link href={"/dashboard"}>Back</Link>
+          </Button>
+        </div>
       </Layout>
     </>
   );
