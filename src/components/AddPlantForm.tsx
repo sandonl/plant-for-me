4e41393dfe2dff9d8486279930dd2be7b71b92b0
Sandon Lai <sandonnlai@gmail.com>
2023-06-07 23:43:00 +1000

    chore: refactor to form component

---
+++ src/components/AddPlantForm.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { Button } from "@/src/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/src/components/ui/form";
+import { Input } from "@/src/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Textarea } from "./ui/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  plantName: z.string().min(2).max(50),
+  water: z.number(),
+  notes: z.string().min(2),
+});
+
+export function AddPlantForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Plant Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Plant Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your personal plant name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="plantName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Scientific Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Plant Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the scientific plant name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* TODO: Fix number validation */}
+        <FormField
+          control={form.control}
+          name="water"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Water (days)</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="7" {...field} />
+              </FormControl>
+              <FormDescription>
+                How often in days do you expect to water your plant
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="notes"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Notes</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Plant Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                Any additional information about your plant
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- src/pages/addplant.tsx
+++ src/pages/addplant.tsx
@@ -1,71 +1,14 @@
-"use client";
-
 import { getSession } from "next-auth/react";
 import Layout from "../components/Layout";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/src/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/src/components/ui/form";
-import { Input } from "@/src/components/ui/input";
+import { AddPlantForm } from "../components/AddPlantForm";
 
 interface addplantProps {}
-
-const formSchema = z.object({
-  name: z.string().min(2).max(50),
-  plantName: z.string().min(2).max(50),
-  water: z.number(),
-  notes: z.string().min(2),
-});
-
-export function AddPlantForm() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Plant Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Plant Name" {...field} />
-              </FormControl>
-              <FormDescription>This is your plant name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
-}
 
 const addplant = ({}: addplantProps) => {
   return (
     <Layout pageTitle="Add Plant">
-      <div className="max-w-8xl w-full">
+      <div className="mx-auto max-w-4xl pt-5 md:w-full">
+        <h1 className="py-3 font-bold"> Add a new plant </h1>
         <AddPlantForm />
       </div>
     </Layout>
