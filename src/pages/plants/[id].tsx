79a280bb23df51d34d01c9ce65e78d056861659a
Sandon Lai <sandonnlai@gmail.com>
2023-06-10 12:40:19 +1000

    fix: individual plant page skeleton views

--- src/pages/plants/[id].tsx
+++ src/pages/plants/[id].tsx
@@ -1,7 +1,9 @@
 import Layout from "@/src/components/Layout";
 import { Button } from "@/src/components/ui/button";
+import { Skeleton } from "@/src/components/ui/skeleton";
 import { api } from "@/src/utils/api";
 import { MoveLeft } from "lucide-react";
+import { getSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -10,15 +12,29 @@ interface PlantPage {}
 const PlantPage = ({}: PlantPage) => {
   const router = useRouter();
   const { id } = router.query;
+  const plantData = api.plant.getPlant.useQuery(
+    { plantId: id as string },
+    { refetchOnWindowFocus: false }
+  );
 
-  const plantData = api.plant.getPlant.useQuery({ plantId: id as string });
-
-  if (plantData.isFetching || plantData.isLoading) {
+  if (
+    plantData.isFetching ||
+    plantData.isLoading ||
+    plantData.isInitialLoading
+  ) {
     return (
       <>
         <Layout pageTitle={"Loading..."}>
           <div className="mx-auto max-w-4xl py-5 md:w-full">
-            <h1 className="text-sm font-bold md:text-3xl">Loading...</h1>
+            <div className="my-1">
+              <Skeleton className="h-10 w-40 bg-slate-300" />
+            </div>
+            <div className="my-3 border-b border-slate-200" />
+
+            <div className="py-1">
+              <Skeleton className="my-2 h-3 w-96 bg-slate-300" />
+              <Skeleton className="my-2 h-3 w-72 bg-slate-300" />
+            </div>
           </div>
         </Layout>
       </>
@@ -45,3 +61,20 @@ const PlantPage = ({}: PlantPage) => {
   );
 };
 export default PlantPage;
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
